d5472706232473e7052e49d0a76e0c38132c2d25
Rishi-Mad <rm08222006@gmail.com>
2025-11-14 01:12:37 -0500

    chore:hello

--- frontend/src/components/common/Carousel.tsx
+++ frontend/src/components/common/Carousel.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { motion, useMotionValue, useAnimationFrame } from 'framer-motion';
 import './Carousel.css';
 
